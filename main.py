97b06636c57b685363658d291e093a47cfe4c865
Michael Surkein <michael@surkein.com>
2021-07-31 13:59:39 -0500

    Removed a few unnecessary headers from the request

--- main.py
+++ main.py
@@ -160,9 +160,7 @@ def generate_new_rom(posted_args):
         s.get("https://alttpr.com/en/randomizer")
         headers = {
             "Content-Type": "application/json;charset=utf-8",
-            "X-Requested-With": "XMLHttpRequest",
-            "Accept": "application/json, text/plain, */*",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0"
+            "Accept": "application/json, text/plain, */*"
         }
         r = s.post("https://alttpr.com/api/randomizer", data=post_data, headers=headers)
         result = r.json()
